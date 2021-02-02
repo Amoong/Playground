5c985b9b2a28a9ae4f1900ef4322470d5a1f6dbc
amoong <moo1323@naver.com>
2021-02-02 21:25:24 +0900

    Add imperfact Todolist

--- ts-context-api-tutorial/src/App.tsx
+++ ts-context-api-tutorial/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
