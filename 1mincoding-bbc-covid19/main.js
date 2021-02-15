3f26ed0ed33fddd7a7596c06f5eb38dc046516a9
amoong <moo1323@naver.com>
2021-02-15 21:25:12 +0900

    covid complete

--- 1mincoding-bbc-covid19/main.js
+++ 1mincoding-bbc-covid19/main.js
@@ -1,4 +1,31 @@
 (() => {
+  const actions = {
+    birdFlies(key) {
+      if (key) {
+        document.querySelector(
+          '[data-index="2"] .bird',
+        ).style.transform = `translateX(${window.innerWidth}px)`;
+      } else {
+        document.querySelector(
+          '[data-index="2"] .bird',
+        ).style.transform = `translateX(-100%)`;
+      }
+    },
+    birdFlies2(key) {
+      if (key) {
+        document.querySelector(
+          '[data-index="5"] .bird',
+        ).style.transform = `translate(${window.innerWidth}px, ${
+          -window.innerHeight * 0.7
+        }px)`;
+      } else {
+        document.querySelector(
+          '[data-index="5"] .bird',
+        ).style.transform = `translateX(-100%)`;
+      }
+    },
+  };
+
   const stepElems = document.querySelectorAll(".step");
   const graphicElems = document.querySelectorAll(".graphic-item");
   let currentItem = graphicElems[0]; // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
@@ -15,12 +42,18 @@
     graphicElems[i].dataset.index = i;
   }
 
-  function activate() {
+  function activate(action) {
     currentItem.classList.add("visible");
+    if (action) {
+      actions[action](true);
+    }
   }
 
-  function inactivate() {
+  function inactivate(action) {
     currentItem.classList.remove("visible");
+    if (action) {
+      actions[action](false);
+    }
   }
 
   window.addEventListener("scroll", () => {
@@ -37,11 +70,17 @@
         boundingRect.top > window.innerHeight * 0.1 &&
         boundingRect.top < window.innerHeight * 0.8
       ) {
-        inactivate();
+        inactivate(currentItem.dataset.action);
         currentItem = graphicElems[i];
-        activate();
+        activate(currentItem.dataset.action);
       }
     }
   });
+
+  window.addEventListener("load", () => {
+    // scrollTo는 살짝 딜레이를 줘야 잘 동작한다.
+    setTimeout(() => scrollTo(0, 0), 100);
+  });
+
   activate();
 })();
